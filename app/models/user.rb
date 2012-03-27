fc63918adb1f1bb8005f754952a2aa29034e3fdd
Achim Taeschner <achim.t@gmail.com>
2012-03-27 18:20:22 +0200

    Authenticates User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessible :name, :email, :password, :password_confirmation
+	has_secure_password
 
+	validates :password, length: { minimum: 6 }
+
+	validates :password_confirmation, presence: true
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
