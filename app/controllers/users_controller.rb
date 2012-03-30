64d632b5e0eaf1e7046bfaa2f963aec33875238c
Achim Taeschner <achim.t@gmail.com>
2012-03-30 19:52:20 +0200

    Only signed-in users are allowed to edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
 	def show
 		@user = User.find(params[:id])
 	end
@@ -33,4 +34,9 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,7 +48,10 @@ describe "UserPages" do
 
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
+  	before do 
+      sign_in user
+      visit user_path(user)
+    end
 
   	it { should have_selector('h1', text: user.name) }
   	it { should have_selector('title', text: user.name) }
@@ -56,7 +59,10 @@ describe "UserPages" do
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do 
+      sign_in user
+      visit edit_user_path(user) 
+    end
 
     describe "page" do
       it { should have_selector 'h1', text: "Update your profile" }
