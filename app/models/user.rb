f8bf605fb32941575c03a0235dbae5db22c78cf0
Achim Taeschner <achim.t@gmail.com>
2012-03-31 06:25:30 +0200

    Associates Micropost with User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
+	has_many :microposts
+
 	before_save :create_remember_token
+
 
 	validates :password, length: { minimum: 6 }
 
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -4,12 +4,28 @@ describe Micropost do
   
   let(:user) { FactoryGirl.create(:user) }
   before do
-  	# This code is wrong!
-  	@micropost = Micropost.new content: "Lorem ipsum", user_id: user.id
+  	@micropost = user.microposts.build content: "Lorem ipsum"
   end
 
   subject { @micropost }
 
   it { should respond_to :content }
   it { should respond_to :user_id }
+  it { should respond_to :user }
+  its(:user) { should == user }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+  	before { @micropost.user_id = nil }
+  	it { should_not be_valid }
+  end
+
+  describe "accessible attributes" do
+  	it "should not allow access to user_id" do
+  		expect do
+  			Micropost.new(user_id: user.id)
+  		end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+  	end
+  end
 end
