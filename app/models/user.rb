babbb7d6a917043b7b74cd9ab440bcffc240d3ef
Achim Taeschner <achim.t@gmail.com>
2012-03-31 06:38:18 +0200

    Destroys microposts if associated user is destroyed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
-	has_many :microposts
+	has_many :microposts, dependent: :destroy
 
 	before_save :create_remember_token
 
