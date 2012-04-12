f8f6544a21f7b3da8085e2244d170005430fe516
Achim Taeschner <achim.t@gmail.com>
2012-04-12 04:46:07 +0200

    follow/unfollow works in the view

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -118,6 +118,18 @@ describe "Authentication" do
 					it { should have_selector 'title', text: 'Sign in' }
 				end
 			end
+
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do
+					before { post relationships_path }
+					specify { response.should redirect_to signin_path }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete relationship_path(1) }
+					specify { response.should redirect_to signin_path }
+				end
+			end
 		end
 
 		describe "as wrong user" do
