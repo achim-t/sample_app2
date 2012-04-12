930b64f69a87a19bcc0315da3286d3271aeebc39
Achim Taeschner <achim.t@gmail.com>
2012-04-12 04:26:39 +0200

    add followers/following to views, only for signed in user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
   before_filter :signed_out_user, only: [:new, :create]
@@ -47,6 +47,20 @@ class UsersController < ApplicationController
     user.destroy unless user == current_user
     flash[:success] = "User destroyed."
     redirect_to users_path
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find params[:id]
+    @users = @user.followed_users.paginate page: params[:page]
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find params[:id]
+    @users = @user.followers.paginate page: params[:page]
+    render 'show_follow'
   end
 
   private
