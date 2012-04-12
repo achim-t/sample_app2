d0a427d771c1b724f3f173af7cbeeaac8a361a89
Achim Taeschner <achim.t@gmail.com>
2012-04-12 02:44:50 +0200

    Follow user/check if other is ofollowing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,8 @@ describe User do
   it { should respond_to :feed }
   it { should respond_to :relationships }
   it { should respond_to :followed_users }
+  it { should respond_to :following? }
+  it { should respond_to :follow! }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -151,6 +153,17 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create :user }
+    before do
+      @user.save
+      @user.follow! other_user
+    end
+
+    it { should be_following other_user }
+    its(:followed_users) { should include other_user }
   end
 
 
