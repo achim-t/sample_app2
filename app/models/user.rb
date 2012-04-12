ecb9776805ce8b9875747279d3f6bb7bee2979b6
Achim Taeschner <achim.t@gmail.com>
2012-04-12 02:48:51 +0200

    Unfollow user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,10 @@ class User < ActiveRecord::Base
 	def follow!(other_user)
 		relationships.create! followed_id: other_user.id
 	end
+
+	def unfollow!(other_user)
+		relationships.find_by_followed_id(other_user).destroy
+	end
 	
 	private
 		def create_remember_token
